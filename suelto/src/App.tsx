310d85a1dc32c7ffb3efd46228ba0b90d2b77127
Joshua <jdib@zeroq.cl>
2024-12-30 16:10:44 -0300

    :add: project base

---
+++ suelto/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import sueltoLogo from './assets/Ilustración_sin_título (2).png'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={sueltoLogo} className="logo" alt="React logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
